53f19ef6a0d0972b38fffc3864b6f0fc895ffd59
MeganTherion <meganhutt@hotmail.com>
2022-09-06 01:56:44 +0000

    add icons aand logic for dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    #count of how many products are in the database
+    #count of how many categories are in the database
+    @product_count = Product.count(:id)
+    @category_count = Category.count(:id)
   end
 end
