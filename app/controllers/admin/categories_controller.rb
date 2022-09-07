3e49fda3f5b09f37df8a2a161b53473a82e5f628
MeganTherion <meganhutt@hotmail.com>
2022-09-07 20:21:25 +0000

    create admin:Categories controller

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,15 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new
+  end
+  
+end
